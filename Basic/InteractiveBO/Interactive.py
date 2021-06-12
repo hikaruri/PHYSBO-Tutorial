94848c58d7e721716953dee06c4a4f8c278d754d
Hikaru Sawahata <45931513+hikaruri@users.noreply.github.com>
2021-06-13 02:40:39 +0900

    Add files via upload

---
+++ Basic/InteractiveBO/Interactive.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+import physbo
+import matplotlib.pyplot as plt
+import random
+
+def load_data():
+    A =  np.asarray(np.loadtxt('data.txt', skiprows=1, delimiter=' ') )
+    X = A[:,0:1]
+    t  = -A[:,1] # BO can search only Maximum value
+    return X, t
+
+X, t = load_data()
+calculated_ids = []
+for i in range(len(t)):
+    if t[i] != 0:
+        calculated_ids.append(i)
+t_initial = t[calculated_ids]
+print(calculated_ids)
+print(t_initial)
+
+# Bayes_search
+policy = physbo.search.discrete.policy(test_X=X, initial_data=[calculated_ids, t_initial])
+actions = policy.bayes_search(max_num_probes=1, simulator=None, score="EI", interval=1,  num_rand_basis = 5000)
+print(actions, X[actions])
