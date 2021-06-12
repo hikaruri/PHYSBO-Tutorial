6c6ee42487be8f4c777f1e3e071fd086e53864fb
Hikaru Sawahata <45931513+hikaruri@users.noreply.github.com>
2021-06-13 01:09:29 +0900

    Add files via upload

--- OneMax.py
+++ OneMax.py
@@ -23,11 +23,31 @@ if __name__ == '__main__':
     test_X = np.array(random.sample(X,len(X)))
 
     policy = physbo.search.discrete.policy(test_X=test_X)
+    
+    # random search
     policy.set_seed(10)
     policy.random_search(max_num_probes=3, simulator=simulator)
     best_fx, best_actions = policy.history.export_sequence_best_fx()
     print(f"best_fx: {best_fx[-1]} at {test_X[best_actions[-1]]}")
-    policy.bayes_search(max_num_probes=10, simulator=simulator, score="EI", 
+
+    # Bayes_search
+    policy.bayes_search(max_num_probes=3, simulator=simulator, score="EI", 
     interval=1, num_rand_basis=500)
+    print(f"best_fx: {best_fx[-1]} at {test_X[best_actions[-1]]}")
+    
+    # Visualization
+    scores = policy.get_score(mode="EI", xs=test_X)
+    fig = plt.figure()
+    plt.plot(scores)
+    fig.savefig("EI_1.png") 
+
+    policy.bayes_search(max_num_probes=7, simulator=simulator, score="EI", 
+    interval=1, num_rand_basis=500)
+
+    scores = policy.get_score(mode="EI", xs=test_X)
+    fig = plt.figure()
+    plt.plot(scores)
+    fig.savefig("EI_2.png")
+
     best_fx, best_actions = policy.history.export_sequence_best_fx()
     print(f"best_fx: {best_fx[-1]} at {test_X[best_actions[-1]]}")
